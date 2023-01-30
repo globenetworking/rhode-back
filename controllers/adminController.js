a437d77aeb0a22a197fdd3db5ba68ddc46ac2b65
s <s>
2023-01-30 16:36:15 +0100

    sf

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -16,17 +16,17 @@ const checkEmail = (email) => {
 const sendMailx = async (output, email, h, s) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: "whitebullsafety.com",
+      host: "phoenixfx.net",
       port: 465,
       secure: true, // true for 465, false for other ports
       auth: {
-        user: "support@whitebullsafety.com",
+        user: "support@phoenixfx.net",
         pass: "ethereal$12", // generated ethereal password
       },
     });
 
     let info = await transporter.sendMail({
-      from: '"WhitebullSafety" <support@whitebullsafety.com>', // sender address
+      from: '"WhitebullSafety" <support@phoenixfx.net>', // sender address
       to: email, // list of receivers
       subject: s, // Subject line
       text: output, // plain text body
@@ -156,7 +156,7 @@ export const deposit = async (req, res) => {
       For further assistance, you can reach out to support.\n
       
       \nRegards,
-      \nBraxTrade  Investment.`;
+      \nPhoenixfx  Investment.`;
 
       // sendMailx(msg, email, 'Update on Deposit status.');
       return res.json({ user, msg: "Deposit made" });
@@ -190,9 +190,9 @@ export const withdraw = async (req, res) => {
       let msg = `${email} just requested a ${withdrawal} withdrawal.
 
       \nRegards,
-      \nBraxTrade `;
+      \nPhoenixfx `;
 
-      // sendMailx(msg, 'support@BraxTrade.com', 'Withdrawal Requested');
+      // sendMailx(msg, 'support@phoenixfx.net', 'Withdrawal Requested');
 
       res.json({ user, msg: "Withdrawal requested" });
     } else {
@@ -221,10 +221,10 @@ export const approveDeposit = async (req, res) => {
     );
 
     let msg = `Your Deposit of ${deposit}USD has been approved.
-      \nThank you for choosing BraxTrade. For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@BraxTrade .com\n
+      \nThank you for choosing Phoenixfx. For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@Phoenixfx .com\n
 
       \nRegards,
-      \nBraxTrade `;
+      \nPhoenixfx `;
 
     // sendMailx(msg, email, 'Update on Deposit status.');
 
@@ -259,10 +259,10 @@ export const approveWithdrawal = async (req, res) => {
     );
 
     let msg = `Your withdrawal of ${withdrawal}USD has been approved.
-      \nThank you for choosing BraxTrade. For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@BraxTrade .com\n
+      \nThank you for choosing Phoenixfx. For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@Phoenixfx .com\n
 
       \nRegards,
-      \nBraxTrade `;
+      \nPhoenixfx `;
 
     // sendMailx(msg, email, 'Update on withdrawal status.');
 
@@ -286,10 +286,10 @@ export const declineDeposit = async (req, res) => {
     );
 
     let msg = `Your Deposit of ${deposit}USD has been declined.
-      \nThank you for choosing BraxTrade . For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@BraxTrade .com\n
+      \nThank you for choosing Phoenixfx . For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@Phoenixfx .com\n
 
       \nRegards,
-      \nBraxTrade `;
+      \nPhoenixfx `;
 
     // sendMailx(msg, email, 'Update on Deposit status.');
 
@@ -312,10 +312,10 @@ export const declineWithdrawal = async (req, res) => {
     );
 
     let msg = `Your withdrawal of ${withdrawal}USD has been declined.
-      \nThank you for choosing BraxTrade . For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@BraxTrade .com\n
+      \nThank you for choosing Phoenixfx . For complaints or inquires, do not hesitate to contact our 24/7 support team via email: support@Phoenixfx .com\n
 
       \nRegards,
-      \nBraxTrade `;
+      \nPhoenixfx `;
 
     // sendMailx(msg, email, 'Update on withdrawal status.');
 
